3e7f8b23682e0c7296b9786767b6306f33ea43a9
kaustubh thapliyal <kaustubhthapliyal@kaustubhs-MacBook-Air.local>
2024-08-22 01:27:35 +0530

    chess game low level design

--- ChessGame-Low-Level-Design/ChessGame.java
+++ ChessGame-Low-Level-Design/ChessGame.java
@@ -1,10 +1,9 @@
-/*
-
-The low-level design for the chess game is still in progress and is not yet completed.
-
-*/
-
-
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayList;
 
 enum PieceType {
     KING,
@@ -13,6 +12,11 @@ enum PieceType {
     KNIGHT,
     BISHOP,
     ROOK
+}
+
+enum Color {
+    WHITE,
+    BLACK
 }
 
 abstract class Piece {
@@ -26,8 +30,16 @@ abstract class Piece {
         this.isAlive = isAlive;
         this.color = color;
     }
+    
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
 
-    public abstract boolean isPossibleMove(Position newPosition, Board board);
+    public abstract boolean isPossibleMove(Position newPosition, ChessBoard board);
 }
 
 class Pawn extends Piece {
@@ -37,7 +49,7 @@ class Pawn extends Piece {
         this.pieceType = PieceType.PAWN;
     }
 
-    public boolean isPossibleMove(Position newPosition, Board board) {
+    public boolean isPossibleMove(Position newPosition, ChessBoard board) {
         if (color == Color.WHITE) {
             return isPossibleWhiteMove(newPosition, board);
         } else {
@@ -45,30 +57,28 @@ class Pawn extends Piece {
         }
     }
 
-    private boolean isPossibleWhiteMove(Position newPosition, Board board) {
-        if (!isWithinBoardBounds(newPosition, board)) return false;
+    private boolean isPossibleWhiteMove(Position newPosition, ChessBoard board) {
 
-        Cell newCell = board.getBoardCell(newPosition.row, newPosition.column);
+        Cell newCell = board.getBoardCell(newPosition);
 
-        if (isForwardMove(newPosition, 1) && newCell.isEmpty()) {
+        if (isForwardMove(newPosition, -1)) {
             return true;
         }
-        if (isCaptureMove(newPosition, 1, newCell, Color.BLACK)) {
+        if (isCaptureMove(newPosition, -1, newCell, Color.BLACK)) {
             return true;
         }
 
         return false;
     }
 
-    private boolean isPossibleBlackMove(Position newPosition, Board board) {
-        if (!isWithinBoardBounds(newPosition, board)) return false;
+    private boolean isPossibleBlackMove(Position newPosition, ChessBoard board) {
 
-        Cell newCell = board.getBoardCell(newPosition.row, newPosition.column);
+        Cell newCell = board.getBoardCell(newPosition);
 
-        if (isForwardMove(newPosition, -1) && newCell.isEmpty()) {
+        if (isForwardMove(newPosition, 1)) {
             return true;
         }
-        if (isCaptureMove(newPosition, -1, newCell, Color.WHITE)) {
+        if (isCaptureMove(newPosition, 1, newCell, Color.WHITE)) {
             return true;
         }
 
@@ -85,12 +95,6 @@ class Pawn extends Piece {
                newCell.getPiece() != null &&
                newCell.getPiece().getColor() == opponentColor;
     }
-
-    private boolean isWithinBoardBounds(Position newPosition, Board board) {
-        int boardSize = board.getBoardSize();
-        return newPosition.row >= 0 && newPosition.row < boardSize &&
-               newPosition.column >= 0 && newPosition.column < boardSize;
-    }
 }
 
 class Rook extends Piece {
@@ -100,7 +104,9 @@ class Rook extends Piece {
         this.pieceType = PieceType.ROOK;
     }
 
-    // rest of the logic will be added here
+    public boolean isPossibleMove(Position newPosition, ChessBoard board) {
+        return true;
+    }
 }
 
 class Queen extends Piece {
@@ -110,7 +116,9 @@ class Queen extends Piece {
         this.pieceType = PieceType.QUEEN;
     }
 
-    // rest of the logic will be added here
+    public boolean isPossibleMove(Position newPosition, ChessBoard board) {
+        return true;
+    }
 }
 
 class King extends Piece {
@@ -120,7 +128,9 @@ class King extends Piece {
         this.pieceType = PieceType.KING;
     }
 
-    // rest of the logic will be added here
+    public boolean isPossibleMove(Position newPosition, ChessBoard board) {
+        return true;
+    }
 }
 
 class Bishop extends Piece {
@@ -130,7 +140,9 @@ class Bishop extends Piece {
         this.pieceType = PieceType.BISHOP;
     }
 
-    // rest of the logic will be added here
+    public boolean isPossibleMove(Position newPosition, ChessBoard board) {
+        return true;
+    }
 }
 
 class Knight extends Piece {
@@ -140,13 +152,307 @@ class Knight extends Piece {
         this.pieceType = PieceType.KNIGHT;
     }
 
-    // rest of the logic will be added here
+    public boolean isPossibleMove(Position newPosition, ChessBoard board) {
+        return true;
+    }
 }
 
+class Position {
+    int row;
+    int column;
+    
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
 
-public class Main {
+class Cell {
+    Position position;
+    Piece piece;
+    
+    public Cell(Position position) {
+        this.position = position;
+        piece = null;
+    }
+    
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+    
+    public void removePiece() {
+        piece = null;
+    }
+    
+    public Piece getPiece() {
+        return piece;
+    }
+    
+    public boolean isEmpty() {
+        return piece == null;
+    }
+}
+
+class ChessBoard {
+    
+    Cell[][] board;
+    int boardSize;
+    
+    public ChessBoard(int boardSize) {
+        this.boardSize = boardSize;
+        board = new Cell[boardSize][boardSize];
+    }
+    
+    public void intializeChessBoard() {
+        
+        for(int row = 0 ; row < boardSize ; row++) {
+            for(int column = 0 ; column < boardSize ; column++) {
+                board[row][column] = new Cell(new Position(row, column));
+            }
+        }
+        
+        setupChessBoard();
+    }
+    
+    public void setupChessBoard() {
+        // Set up black pieces
+        board[0][0].setPiece(new Rook(new Position(0, 0), true, Color.BLACK));
+        board[0][1].setPiece(new Knight(new Position(0, 1), true, Color.BLACK));
+        board[0][2].setPiece(new Bishop(new Position(0, 2), true, Color.BLACK));
+        board[0][3].setPiece(new Queen(new Position(0, 3), true, Color.BLACK));
+        board[0][4].setPiece(new King(new Position(0, 4), true, Color.BLACK));
+        board[0][5].setPiece(new Bishop(new Position(0, 5), true, Color.BLACK));
+        board[0][6].setPiece(new Knight(new Position(0, 6), true, Color.BLACK));
+        board[0][7].setPiece(new Rook(new Position(0, 7), true, Color.BLACK));
+    
+        // Set up black pawns
+        for (int i = 0; i < boardSize; i++) {
+            board[1][i].setPiece(new Pawn(new Position(1, i), true, Color.BLACK));
+        }
+    
+        // Set up white pieces
+        board[7][0].setPiece(new Rook(new Position(7, 0), true, Color.WHITE));
+        board[7][1].setPiece(new Knight(new Position(7, 1), true, Color.WHITE));
+        board[7][2].setPiece(new Bishop(new Position(7, 2), true, Color.WHITE));
+        board[7][3].setPiece(new Queen(new Position(7, 3), true, Color.WHITE));
+        board[7][4].setPiece(new King(new Position(7, 4), true, Color.WHITE));
+        board[7][5].setPiece(new Bishop(new Position(7, 5), true, Color.WHITE));
+        board[7][6].setPiece(new Knight(new Position(7, 6), true, Color.WHITE));
+        board[7][7].setPiece(new Rook(new Position(7, 7), true, Color.WHITE));
+    
+        // Set up white pawns
+        for (int i = 0; i < boardSize; i++) {
+            board[6][i].setPiece(new Pawn(new Position(6, i), true, Color.WHITE));
+        }
+    }
+
+    
+    public void move(Position currentPosition, Position nextPosition) {
+        
+        Cell currentCell = board[currentPosition.row][currentPosition.column];
+        Cell nextCell = board[nextPosition.row][nextPosition.column];
+        
+        Piece piece = currentCell.getPiece();
+        Piece nextCellPiece = nextCell.getPiece();
+        
+        nextCell.setPiece(piece);
+        currentCell.removePiece();
+        piece.setPosition(nextPosition);
+        
+    }
+
+    public void printChessBoard() {
+        for (int row = 0; row < boardSize; row++) {
+            for (int column = 0; column < boardSize; column++) {
+                Piece piece = board[row][column].getPiece();
+                if (piece == null) {
+                    System.out.print("- ");
+                } else {
+                    switch (piece.pieceType) {
+                        case KING:
+                            System.out.print((piece.getColor() == Color.WHITE ? "K " : "k "));
+                            break;
+                        case QUEEN:
+                            System.out.print((piece.getColor() == Color.WHITE ? "Q " : "q "));
+                            break;
+                        case ROOK:
+                            System.out.print((piece.getColor() == Color.WHITE ? "R " : "r "));
+                            break;
+                        case BISHOP:
+                            System.out.print((piece.getColor() == Color.WHITE ? "B " : "b "));
+                            break;
+                        case KNIGHT:
+                            System.out.print((piece.getColor() == Color.WHITE ? "N " : "n "));
+                            break;
+                        case PAWN:
+                            System.out.print((piece.getColor() == Color.WHITE ? "P " : "p "));
+                            break;
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    
+    public Cell getBoardCell(Position position) {
+        return board[position.row][position.column];
+    }
+    
+    public int getBoardSize() {
+        return boardSize;
+    }
+    
+}
+
+class Player {
+    
+    String playerName;
+    int playerRank;
+    Color playerColor;
+    
+    public Player(String playerName, int playerRank, Color playerColor) {
+        this.playerName = playerName;
+        this.playerRank = playerRank;
+        this.playerColor = playerColor;
+    }
+    
+    public Color getPlayerColor() {
+        return playerColor;
+    }
+    
+    public String getName() {
+        return playerName;
+    }
+}
+
+class PlayChessGame {
+    
+    ChessBoard chessBoard;
+    Deque<Player> playersList = new LinkedList<>();
+    Player winner;
+    
+    public PlayChessGame(int boardSize, List<Player> players) {
+        intializeGame(boardSize, players);
+    }
+    
+    public void intializeGame(int boardSize, List<Player> players) {
+        chessBoard = new ChessBoard(boardSize);
+        chessBoard.intializeChessBoard();
+        
+        for(Player player : players) {
+            playersList.add(player);
+        }
+    }
+    
+    public boolean checkMoveValidation(Position currentPosition, Position nextPosition) {
+        
+        int boardSize = chessBoard.getBoardSize();
+        boolean boardValidation = true;
+        boolean cellValidation = true;
+        boolean pieceValidation = true;
+        
+        if(currentPosition.row >= boardSize || currentPosition.column >= boardSize || nextPosition.row >= boardSize &&  nextPosition.column >= boardSize) {
+                boardValidation = false;
+        }
+        
+        Cell currentCell = chessBoard.getBoardCell(currentPosition);
+        Cell nextCell = chessBoard.getBoardCell(nextPosition);
+        
+        if(currentCell == null) {
+            cellValidation = false;
+        }
+        
+        Piece piece = currentCell.getPiece();
+        Piece nextCellPiece = nextCell.getPiece();
+        
+        if(piece == null || (nextCellPiece != null && nextCellPiece.color == piece.color)) {
+            pieceValidation = false;
+        }
+        
+        boolean moveValidation = piece.isPossibleMove(nextPosition, chessBoard);
+
+        return boardValidation && cellValidation && pieceValidation && moveValidation;
+    }
+    
+    public void startGame() {
+        
+        while(winner == null) {
+            
+            // get current player turn
+            Player currentPlayer = playersList.removeFirst();
+            
+            // show board structure
+            chessBoard.printChessBoard();
+            
+            System.out.println("current player : " + currentPlayer.getName());
+            
+            // read player value
+            System.out.println("Player: " + currentPlayer.getName() + " enter your move\n");
+            Scanner inputScanner = new Scanner(System.in);
+            String moveInput = inputScanner.nextLine();
+            
+            if(moveInput.equals("end game")) {
+                winner = playersList.removeFirst();
+                continue;
+            }
+            
+            // Split the input into two parts: "x1,y1" and "x2,y2"
+            String[] positions = moveInput.split(" ");
+            // Further split each part by the comma to get row and column values
+            String[] fromPositionValues = positions[0].split(",");
+            int fromRow = Integer.valueOf(fromPositionValues[0]);
+            int fromColumn = Integer.valueOf(fromPositionValues[1]);
+            Position currentPosition = new Position(fromRow, fromColumn);
+            String[] toPositionValues = positions[1].split(",");
+            int toRow = Integer.valueOf(toPositionValues[0]);
+            int toColumn = Integer.valueOf(toPositionValues[1]);
+            Position nextPosition = new Position(toRow, toColumn);
+            
+            // check if the player move is valid or not
+            if(!checkMoveValidation(currentPosition, nextPosition)) {
+                System.out.println("Player : " + currentPlayer.getName() + " invalid move try again\n");
+                playersList.addFirst(currentPlayer);
+                continue;
+            }
+            
+            // make a move in chess
+            chessBoard.move(currentPosition, nextPosition);
+            
+            // update player turn
+            playersList.addLast(currentPlayer);
+            
+        }
+        
+        System.out.println("winner of the game is " + winner.getName());
+        
+    }
+}
+
+public class ChessGame {
 	public static void main(String[] args) {
         
-		System.out.println("The low-level design for the chess game is still in progress and is not yet completed.");
+		System.out.println("low level design of chess game\n\n");
+		System.out.println("game instructions\n");
+		System.out.println("1. if you want to make a move enter from which position you want to move to which position");
+		System.out.println("   enter the move in given format x1,y1 x2,y2");
+		System.out.println("2. if lose the game enter 'end game'");
+		System.out.println("3. in chess board white pieces is on top of board and black pieces are on bottom");
+		System.out.println("4. white pieces are denoted in lowercase and black pieces are denoted in uppercase\n");
+		
+		// get the board size
+		int boardSize = 8;
+		
+		// create players
+		Player player1 = new Player("jimmy", 1, Color.WHITE);
+		Player player2 = new Player("josh", 1, Color.BLACK);
+		List<Player> players = new ArrayList<>();
+		players.add(player1);
+		players.add(player2);
+		
+		PlayChessGame playChessGame = new PlayChessGame(boardSize, players);
+		
+		playChessGame.startGame();
+		
 	}
 }
--- ChessGame-Low-Level-Design/README.md
+++ ChessGame-Low-Level-Design/README.md
@@ -1,32 +1,67 @@
 # Chess Game Low-Level Design
 
+This repository contains the low-level design of a chess game implemented in Java. The design models a chess game with a focus on object-oriented principles, where each piece has its own movement logic, and the game board manages the state of the game.
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Features](#features)
+- [Classes and Structure](#classes-and-structure)
+- [Game Instructions](#game-instructions)
+- [How to Run](#how-to-run)
+- [Future Enhancements](#future-enhancements)
+
 ## Overview
 
-This repository contains the low-level design of a Chess game, implemented in Java. The design includes various components necessary to simulate a standard Chess game, including classes for different pieces, the game board, players, and game logic. This design emphasizes object-oriented principles and design patterns to ensure modularity, scalability, and maintainability.
+This chess game implementation provides a basic framework to simulate a standard chess game. It covers essential features such as board setup, piece movement, and player turns. The code is structured to separate concerns between different classes, making it easy to extend and modify.
 
 ## Features
 
-- **Object-Oriented Design**: Utilizes object-oriented principles to model the Chess game, with clear abstractions for pieces, moves, and game state.
-- **Game Board**: A class to represent the chessboard with an 8x8 grid, managing the positions of the pieces.
-- **Chess Pieces**: Separate classes for each type of piece (Pawn, Rook, Knight, Bishop, Queen, King), encapsulating their movement logic.
-- **Game Logic**: Classes to handle the rules of Chess, including check, checkmate, and special moves like castling and en passant.
-- **Players**: Models players with their respective pieces and the ability to make moves.
-- **Move Validation**: Ensures that moves adhere to the rules of Chess, including legal and illegal moves.
-- **Turn Management**: Handles alternating turns between the two players.
+- **Chess Board Setup:** The game initializes an 8x8 chessboard with all the pieces placed in their starting positions.
+- **Piece Movement:** Each piece type (Pawn, Rook, Knight, Bishop, Queen, King) has its own movement logic. For example, Pawns can move forward or capture diagonally.
+- **Move Validation:** The game validates player moves based on piece rules and ensures that moves are within the boundaries of the board.
+- **Player Turns:** The game alternates turns between two players and validates their inputs.
+- **Game Termination:** Players can end the game at any point, and the winner is declared based on the game state.
 
-## Design Patterns Used
+## Classes and Structure
 
-- **Factory Pattern**: Used for creating different chess pieces based on the type and color.
-- **Strategy Pattern**: Implements the movement behavior of each piece, allowing flexibility in handling piece-specific rules.
-- **Observer Pattern**: Notifies components of changes in the game state, such as when a move is made or when checkmate occurs.
-- **Command Pattern**: Encapsulates a move as an object, allowing undo/redo functionality if implemented in the future.
+### 1. `Piece`
+- Abstract base class representing a generic chess piece.
+- Derived classes: `Pawn`, `Rook`, `Knight`, `Bishop`, `Queen`, `King`.
+- Each derived class implements the `isPossibleMove()` method to define valid moves.
 
-## Classes
+### 2. `Position`
+- Represents the position of a piece on the chessboard with `row` and `column` attributes.
 
-- **Game**: Manages the flow of the game, including starting, stopping, and tracking the state.
-- **Board**: Represents the chessboard and manages piece positions.
-- **Piece**: Abstract base class for all chess pieces.
-- **Specific Piece Classes**: (Pawn, Rook, Knight, Bishop, Queen, King) - Handle the unique movement logic for each piece.
-- **Player**: Represents a player in the game, managing their pieces and moves.
-- **Move**: Represents a move on the board, including validation logic.
-- **GameState**: Tracks the current state of the game, including whose turn it is, and whether the game is in check or checkmate.
+### 3. `Cell`
+- Represents a single cell on the chessboard.
+- Manages the piece occupying the cell and provides methods to set or remove a piece.
+
+### 4. `ChessBoard`
+- Manages the chessboard as a 2D array of `Cell` objects.
+- Responsible for initializing the board, placing pieces, and handling piece movements.
+
+### 5. `Player`
+- Represents a player in the game with attributes like `playerName`, `playerRank`, and `playerColor`.
+
+### 6. `PlayChessGame`
+- Coordinates the game flow, including player turns, move validation, and checking for game termination.
+
+### 7. `ChessGame`
+- Main class to start and run the chess game.
+
+## Game Instructions
+
+1. **Move Format:** To make a move, enter the starting position and the target position in the format `x1,y1 x2,y2`. For example, `1,0 2,0` moves the piece from row 1, column 0 to row 2, column 0.
+2. **End Game:** If you want to end the game, type `"end game"`.
+3. **Chess Board Orientation:**
+   - White pieces are at the top of the board, represented by lowercase letters.
+   - Black pieces are at the bottom of the board, represented by uppercase letters.
+
+## Future Enhancements
+
+- `Add More Piece Movement Logic:` Implement detailed movement rules for all pieces (e.g., castling for the King, en passant for Pawns).
+- `Game Status Checks:` Implement checks for check, checkmate, and stalemate.
+- `Graphical User Interface (GUI):` Develop a graphical interface to enhance user experience.
+- `AI Opponent:` Implement a basic AI to play against the computer.
+
