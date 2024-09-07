cdb2cfc6d85a29861dd2a82de165ee9da1131554
kaustubh thapliyal <kaustubhthapliyal245@gmail.com>
2024-09-07 17:54:32 +0530

    airline management system

---
+++ AirlineManagementSystem-Low-Level-Design/AirlineManagementSystem.java
@@ -0,0 +1,17 @@
+/*
+
+1. flights -> fare details
+2. flight manager -> each airline has a flight manager that maintains list of flights
+3. airline -> manages flights and reservations
+4. airline manager -> manages all the available airlines, and reterive user needed flights.
+5. user
+6. reservation
+7. booking
+8. payment
+9. notification
+10. seat -> user decorator design pattern for calculating seat price 
+
+*/
+
+
+// need to implement the code.
