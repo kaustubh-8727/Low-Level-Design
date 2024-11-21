51dd731bce61dcf1da82f525526c0157b676256e
kaustubh thapliyal <kaustubhthapliyal245@gmail.com>
2024-11-22 00:22:01 +0530

    stock brokerage system

--- Stock-Brokerage-System-Low-Level-Design/StockBrokerageSystem.java
+++ Stock-Brokerage-System-Low-Level-Design/StockBrokerageSystem.java
@@ -12,6 +12,7 @@ Stock Brokerage System
 9. sell order class.
 10. order management - manages all the orders (stores orders in queue to process), call specific order to execute.
 11. stock market service - singleton class that performs the actual actions.
+12. stock filter - a stock filter which will return the stocks based on filters applied.
 
 */
 
@@ -62,10 +63,26 @@ class UserAccount {
     }
 }
 
+class UserStock {
+    Stock stock;
+    double quantity;
+    double priceBought;
+    
+    public UserStock(Stock stock, double quantity, double priceBought) {
+        this.stock = stock;
+        this.quantity = quantity;
+        this.priceBought = priceBought;
+    }
+    
+    public double getQuantity() {
+        return quantity;
+    }
+}
+
 class Portfolio {
     
     String portfolioId;
-    Map<String, Order> stocksHolding = new HashMap<>();
+    Map<String, UserStock> stocksHolding = new HashMap<>();
     Map<String, Order> orderHistory = new HashMap<>();
     double currentProfit;
     double totalAmountInvested;
@@ -76,15 +93,28 @@ class Portfolio {
     }
     
     public void buyStockUpdate(Order order) {
-        stocksHolding.put(order.getOrderId(), Order);
+        UserStock userStock = createUserStock(order);
+        stocksHolding.put(order.getStock().getStockId(), userStock);
         orderHistory.put(order.getOrderId(), Order);
         
         this.updatePortfolioBalance();
     }
     
+    public UserStock createUserStock(Order order) {
+        Stock stock = order.getStock();
+        double quantity = order.getQuantity();
+        double price = stock.getStockValue();
+        
+        UserStock userStock = new UserStock(stock, quantity, price);
+        
+        return userStock;
+    }
+    
     public void sellStockUpdate(Order order) {
-        if(stocksHolding.containsKey(order.getOrderId())) {
-            stocksHolding.remove(order.getOrderId());
+        String stockId = order.getStock().getStockId();
+        
+        if(stocksHolding.containsKey(stockId)) {
+            stocksHolding.remove(stockId);
         }
         if(orderHistory.containsKey(order.getOrderId())) {
             orderHistory.remove(order.getOrderId());
@@ -92,6 +122,16 @@ class Portfolio {
         
         this.updatePortfolioBalance();
     }
+    
+    public Stock getStock(Stock stock) {
+        string stockId = stock.getStockId();
+        
+        if(!stocksHolding.containsKey(stockId)) {
+            return null;
+        }
+        
+        return stocksHolding.get(stockId);
+    } 
     
     public void updatePortfolioBalance() {
         // this class will update currentProfit, currentLoss, totalAmountInvested
@@ -113,6 +153,134 @@ class UserManagement {
     public UserAccount getUserAccount(String userId) {
         UserAccount userAccount = getUserById(userId);
         return userAccount;
+    }
+}
+
+class Stock {
+    private String stockId;
+    private String stockName;
+    private double stockValue;
+
+    public Stock(String stockName, double stockValue) {
+        this.stockId = "1234";
+        this.stockName = stockName;
+        this.stockValue = stockValue;
+    }
+
+    public String getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(String stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public double getStockValue() {
+        return stockValue;
+    }
+
+    public void setStockValue(double stockValue) {
+        this.stockValue = stockValue;
+    }
+}
+
+class StockManagement {
+    List<Stock> stocksList = new ArrayList<>();
+    
+    public void addStock(Stock stock) {
+        stocksList.add(stock);
+    }
+    
+    public void removeStokc(Stock stock) {
+        stocksList.remove(stock);
+    }
+    
+    public List<Stock> getStocks(StockFilter stockFilter) {
+        if(stockFilter == null) {
+            return stockList;
+        }
+        return stockList;
+    }
+}
+
+class StockFilter {
+    
+}
+
+abstract class Order {
+    String orderId;
+    UserAccount userAccount;
+    Stock stock;
+    double quantity;
+    
+    public Order(UserAccount userAccount, Stock stock) {
+        this.orderId = "1234";
+        this.userAccount = userAccount;
+        this.stock = stock;
+        this.quantity = quantity;
+    }
+    
+    public Stock getStock() {
+        return stock;
+    }
+    
+    public void processOrder();
+}
+
+class BuyOrder extends Order {
+    
+    double price = quantity * stock.getStockValue();
+    
+    public void prodessOrder() {
+        
+        if(this.validateOrder() == false) {
+            // throw exception
+        }
+        
+        userAccount.debitBalance(totalPrice);
+        userAccount.getPortfolio().buyStockUpdate(Order)
+    }
+    
+    public boolean validateOrder() {
+        boolean isValid = true;
+        if(userAccount.getBalance() < price) {
+            isValid = false;
+        }
+        return isValid;
+    }
+}
+
+class SellOrder extends Order {
+    double price = quantity * stock.getStockValue();
+    
+    public void prodessOrder() {
+        
+        if(this.validateOrder() == false) {
+            // throw exception
+        }
+        userAccount.creditBalance(totalPrice);
+        userAccount.getPortfolio().sellStockUpdate(Order);
+    }
+    
+    public boolean validateOrder() {
+        boolean isValid = true;
+        
+        if(userAccount.getPortfolio().getStock(this.order.getStock()) == null) {
+            isValid = false;
+        }
+        
+        if(order.getQuantity() > userAccount.getPortfolio().getStock(order.getStock().getStockId()).getQuantity()) {
+            isValid = false;
+        }
+        return isValid;
     }
 }
 
