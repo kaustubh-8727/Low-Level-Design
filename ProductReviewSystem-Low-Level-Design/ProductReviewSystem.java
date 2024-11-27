a0297496eabb34043dd18605b24267a370ab0817
kaustubh thapliyal <kaustubhthapliyal245@gmail.com>
2024-11-28 00:32:52 +0530

    product review system low level design

---
+++ ProductReviewSystem-Low-Level-Design/ProductReviewSystem.java
@@ -0,0 +1,15 @@
+/*
+
+product review system
+
+1. user
+2. user management
+3. review
+4. review summary
+5. review service
+6. product
+7. product service
+8. review type enum
+9. review filter
+
+*/
